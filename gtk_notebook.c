b82290de59519ca27bd272d5ba07dd12b26a8063
Quiddity <nick.sexton@gmail.com>
2014-03-02 14:16:24 +0000

    two page notebook

--- gtk_notebook.c
+++ gtk_notebook.c
@@ -1,7 +1,46 @@
 #include <gtk/gtk.h>
 
+static GtkWidget* create_notepage_zinc() {
+
+  GtkWidget *grid;
+  GtkWidget *label1;
+
+  label1 = gtk_label_new("Zinc is a moderately reactive, blue gray metal \
+that tarnishes in moist air and burns in air with a bright blueish-green flame,\
+giving off fumes of zinc oxide. It reacts with acids, alkalis and other non-metals.\
+If not completely pure, zinc reacts with dilute acids to release hydrogen.");
+
+  gtk_label_set_line_wrap(GTK_LABEL(label1), TRUE);
+
+  grid = gtk_grid_new();
+  gtk_grid_attach (GTK_GRID(grid), label1, 0, 0, 1, 1);
+
+  gtk_widget_show_all(grid);
+  return (grid);
+
+}
 
 
+static GtkWidget* create_notepage_copper() {
+
+  GtkWidget *grid;
+  GtkWidget *label1;
+
+  label1 = gtk_label_new("Copper is an essential trace nutrient to all high \
+plants and animals. In animals, including humans, it is found primarily in \
+the bloodstream, as a co-factor in various enzymes, and in copper-based pigments. \
+However, in sufficient amounts, copper can be poisonous and even fatal to organisms.");
+
+  gtk_label_set_line_wrap(GTK_LABEL(label1), TRUE);
+
+  grid = gtk_grid_new();
+  gtk_grid_attach (GTK_GRID(grid), label1, 0, 0, 1, 1);
+
+  gtk_widget_show_all(grid);
+  return (grid);
+
+}
+  
 static void activate(GtkApplication *app, gpointer user_data) {
 
   GtkWidget *window;
@@ -9,6 +48,9 @@ static void activate(GtkApplication *app, gpointer user_data) {
 
   GtkWidget *toolbar;
   GtkToolItem *tool_item;
+
+  GtkWidget *notes;
+
 
   int position = 0; // toolbar position
 
@@ -20,6 +62,7 @@ static void activate(GtkApplication *app, gpointer user_data) {
   gtk_container_set_border_width (GTK_CONTAINER(window), 10);
   
 
+  // ------------- TOOLBAR ----------------
   // Create a basic toolbar
   toolbar = gtk_toolbar_new();
   gtk_toolbar_set_style(GTK_TOOLBAR(toolbar), GTK_TOOLBAR_ICONS);
@@ -33,16 +76,28 @@ static void activate(GtkApplication *app, gpointer user_data) {
   gtk_toolbar_insert(GTK_TOOLBAR(toolbar), tool_item, position ++);
 
   tool_item = gtk_tool_button_new_from_stock (GTK_STOCK_CLOSE);
-  // connect button callback here!
+  // connect application quit callback here
   gtk_toolbar_insert(GTK_TOOLBAR(toolbar), tool_item, position ++);
 
+
+  // ------------- NOTEPAD -----------------
+
+  notes = gtk_notebook_new();
+  gtk_notebook_append_page(GTK_NOTEBOOK(notes), 
+			   create_notepage_zinc(), 
+			   gtk_label_new("Zinc"));
+  gtk_notebook_append_page(GTK_NOTEBOOK(notes), 
+			   create_notepage_copper(), 
+			   gtk_label_new("Copper"));
 
 
   // Create a full-window grid to contain toolbar and the notebook
   grid = gtk_grid_new();
-  gtk_grid_attach (GTK_GRID(grid), toolbar, 1, 1, 1, 1);
+  gtk_grid_attach (GTK_GRID(grid), toolbar, 0, 0, 1, 1);
+  gtk_grid_attach (GTK_GRID(grid), notes, 0, 1, 1, 1);
 
   gtk_container_add (GTK_CONTAINER(window), GTK_WIDGET(grid));
+
 
   gtk_widget_show_all (window);
 }

9e61a97b7b33328c29037066699d9a8c69bd50e5
Quiddity <nick.sexton@gmail.com>
2014-03-02 13:45:46 +0000

    created basic toolbar

--- gtk_notebook.c
+++ gtk_notebook.c
@@ -5,15 +5,44 @@
 static void activate(GtkApplication *app, gpointer user_data) {
 
   GtkWidget *window;
+  GtkWidget *grid;
 
+  GtkWidget *toolbar;
+  GtkToolItem *tool_item;
+
+  int position = 0; // toolbar position
 
   // Create a window with a title, default size, and set border width
   window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW(window), "Grid example");
+  gtk_window_set_title (GTK_WINDOW(window), "GUI: notebook");
+  gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
+  gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+  gtk_container_set_border_width (GTK_CONTAINER(window), 10);
+  
 
-  // gtk_window_set_default_size(GTK_WINDOW(window), 300, 100);
-  // gtk_container_set_border_width (GTK_CONTAINER(window), 10);
+  // Create a basic toolbar
+  toolbar = gtk_toolbar_new();
+  gtk_toolbar_set_style(GTK_TOOLBAR(toolbar), GTK_TOOLBAR_ICONS);
 
+  tool_item = gtk_tool_button_new_from_stock (GTK_STOCK_REFRESH);
+  // connect button callback here!
+  gtk_toolbar_insert(GTK_TOOLBAR(toolbar), tool_item, position ++);
+
+  tool_item = gtk_separator_tool_item_new();
+  gtk_separator_tool_item_set_draw(GTK_SEPARATOR_TOOL_ITEM(tool_item), FALSE);
+  gtk_toolbar_insert(GTK_TOOLBAR(toolbar), tool_item, position ++);
+
+  tool_item = gtk_tool_button_new_from_stock (GTK_STOCK_CLOSE);
+  // connect button callback here!
+  gtk_toolbar_insert(GTK_TOOLBAR(toolbar), tool_item, position ++);
+
+
+
+  // Create a full-window grid to contain toolbar and the notebook
+  grid = gtk_grid_new();
+  gtk_grid_attach (GTK_GRID(grid), toolbar, 1, 1, 1, 1);
+
+  gtk_container_add (GTK_CONTAINER(window), GTK_WIDGET(grid));
 
   gtk_widget_show_all (window);
 }
